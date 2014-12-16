9c0e98d80a894c0c73aa12bbe1034c4d03384642
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2014-12-16 18:16:39 +0200

    m

--- resources/init.php
+++ resources/init.php
@@ -8,7 +8,7 @@
 
 // should be refactored to use constants from config.php
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-$db = new PDO("mysql:host=localhost;dbname=blog_project;charset=utf8", 'root', 'root', $options);
+$db = new PDO("mysql:host=localhost;dbname=blog_project;charset=utf8", 'root', '', $options);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
