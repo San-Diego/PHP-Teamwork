5eb8b2babba65f55278217162356565564ccae6a
krastoss <servicemail945@gmail.com>
2014-12-16 15:06:35 +0200

    menu styled

--- views/elements/header.php
+++ views/elements/header.php
@@ -1,17 +1,16 @@
 <header>
-    <h1>This is our header!</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Index</a></li>
-            <li><a href="add_post.php">Add a Post</a></li>
-            <li><a href="add_category.php">Add a Category</a></li>
-            <li><a href="category_list.php">Category List</a></li>
-        </ul>
-    </nav>
-    <?php if(isset($_SESSION["user"])): ?>
-    <div>
-        <a href="logout.php">Log out</a><br />
-        <span>User name: <?php echo $_SESSION["user"]["username"] ?></span>
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="index.php">Home</a>
+          <a class="blog-nav-item" href="add_post.php">Add a Post</a>
+          <a class="blog-nav-item" href="add_category.php">Add a Category</a>
+          <a class="blog-nav-item" href="category_list.php">Category List</a>
+		  <?php if(isset($_SESSION["user"])): ?>
+          <a class="blog-nav-item" href="logout.php">Log out</a>
+		  <span>User name: <?php echo $_SESSION["user"]["username"] ?></span>
+		  <?php endif ?>
+        </nav>
+      </div>
     </div>
-    <?php endif ?>
 </header>
--- views/templates/default_template.php
+++ views/templates/default_template.php
@@ -1,19 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1"/>
-    <style>
-        ul {
-            list-style-type: none;
-        }
-        li {
-            display: inline;
-            margin-right: 20px;
-        }
-    </style>
-    <title>Team San Diego's Blog</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Blog Team San Diego</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../lib/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../../lib/css/main.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../lib/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 <body>
     <?php include_once DX_ROOT_DIR . 'views/elements/header.php'; ?>
     <?php include_once DX_ROOT_DIR . $element ?>
