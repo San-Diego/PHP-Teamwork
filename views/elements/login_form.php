5b3227e36266f0be0e624fe4265b81689349052c
Krastyo Krastev <servicemail945@gmail.com>
2014-12-17 13:19:08 +0200

    styled login and register
    Conflicts:
    	views/elements/login_form.php

--- views/elements/login_form.php
+++ views/elements/login_form.php
@@ -1,10 +1,8 @@
 <div class="container">
     <form method="post" class="form-signin" role="form">
             <h2 class="form-signin-heading">Please log in</h2>
-            <label for="inputEmail" class="sr-only">Email address</label>
             <input type="text" id="username" name="username" class="form-control" placeholder="User name" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
             <input class="btn btn-lg btn-primary btn-block" type="submit" value="Log in"/>
     </form>
 </div> <!-- /container -->
--- views/elements/register_form.php
+++ views/elements/register_form.php
@@ -1,10 +1,9 @@
-<h3>Register</h3>
-<form method="POST" action="">
-    <label>User name:</label>
-    <input type="text" name="username"/><br />
-    <label>Password:</label>
-    <input type="password" name="password" /><br />
-    <label>Email:</label>
-    <input type="email" name="email" /><br/>
-    <input type="submit" value="Register"/><br />
-</form>
+<div class="container">
+    <form method="post" class="form-signin" role="form">
+        <h2 class="form-signin-heading">Register</h2>
+        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="E-mail" required>
+        <input type="text" id="username" name="username" class="form-control" placeholder="User name" required autofocus>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Log in"/>
+    </form>
+</div> <!-- /container -->
