0a133f90e58059e9e019e5eff6522203c9be9efa
Krastyo Krastev <servicemail945@gmail.com>
2014-12-17 13:41:17 +0200

    fix

--- views/elements/register_form.php
+++ views/elements/register_form.php
@@ -3,7 +3,7 @@
         <h2 class="form-signin-heading">Register</h2>
         <input type="email" id="inputEmail" name="email" class="form-control" placeholder="E-mail" required autofocus>
         <input type="text" id="username" name="username" class="form-control" placeholder="User name" required>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Log in"/>
+        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Register"/>
     </form>
 </div> <!-- /container -->
